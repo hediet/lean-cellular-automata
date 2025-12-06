22a2ee1b4ff1f3a5f7f8f854d504eda1a3db3840
Henning Dieterichs <henning.dieterichs@live.de>
2025-12-06 22:11:20 +0000

    rename

--- CellularAutomatas.lean
+++ CellularAutomatas.lean
@@ -4,4 +4,4 @@ import CellularAutomatas.examples
 import CellularAutomatas.results
 import CellularAutomatas.results_unproven
 import CellularAutomatas.proofs.common
-import CellularAutomatas.proofs.prefix_stable_of_rt_closed
+import CellularAutomatas.proofs.is_two_stage_of_rt_closed_and_prefix_stable
